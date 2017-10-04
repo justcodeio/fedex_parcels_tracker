2b314cc2e4f4871dca7e9f37373a54b5397e82cd
Michal Andros <michalandros@gmail.com>
2017-10-04 14:43:06 +0200

    tracker v - 1

--- lib/fedex_parcels_tracker.rb
+++ lib/fedex_parcels_tracker.rb
@@ -1,5 +1,65 @@
-require "fedex_parcels_tracker/version"
+require 'fedex_parcels_tracker/version'
+require 'savon'
 
-module FedexParcelsTracker
-  # Your code goes here...
+module Fedex
+  class Parcel
+    attr_accessor :client
+
+    class << self
+      attr_accessor :configuration
+    end
+
+    def self.configure
+      self.configuration ||= Configuration.new
+      yield(configuration)
+    end
+
+    class Configuration
+      attr_accessor :access_key, :wsdl, :method, :data, :tracking_in_data
+
+      def initialize
+        @wsdl = nil
+        @access_key = nil
+        @method = nil
+        @tracking_in_data = nil
+        @data = {}
+      end
+    end
+
+    def initialize
+      @client = Savon.client(
+        wsdl:        Fedex::Parcel.configuration.wsdl,
+        ssl_version: :TLSv1
+      )
+    end
+
+    def self.track(tracking_code)
+      raise 'Tracking code cannot be blank' if tracking_code.blank?
+      raise 'Invalid tracking code provided' unless (12..14).cover? tracking_code.length
+
+      resp = self.new.client.call(
+        Fedex::Parcel.configuration.method,
+        message: self.assign_tracking_code(tracking_code)
+      )
+
+      body = resp.body
+      result = self.body_contents(body)
+      if resp.http.code == 200 && result.to_s.length < 90
+        return { error: 'Tracking code provided no longer found' }
+      else
+        result
+      end
+    end
+
+    def self.assign_tracking_code(tracking_code)
+      tracking_in_data = Fedex::Parcel.configuration.tracking_in_data
+      data = Fedex::Parcel.configuration.data
+      data[tracking_in_data.to_sym] = tracking_code
+      data
+    end
+
+    def self.body_contents(body)
+      body[(Fedex::Parcel.configuration.method.to_s + '_response').to_sym]
+    end
+  end
 end
